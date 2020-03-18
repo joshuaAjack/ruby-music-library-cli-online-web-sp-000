43e8d4f30492c6abbbe72ef26d057b77696171ca
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-03-18 03:57:54 +0000

    Automatically backed up by Learn

---
+++ lib/Artist.rb
@@ -0,0 +1,46 @@
+require 'pry'
+# require_relative "../config/environment.rb"
+
+class Artist
+  extend Concerns::Findable
+  attr_accessor :name
+  attr_reader :songs
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+    # self.save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    artist = self.new(name)
+    artist.save
+    artist
+  end
+
+  def songs
+    @songs
+  end
+
+  def add_song(song)
+
+      song.artist = self unless song.artist
+      songs << song unless songs.include?(song)
+      # @songs.uniq!
+
+  end
+end
